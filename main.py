bcd17665b31eba8fbbfe402fef71e2e01af19ee8
DanielRB <trynd3@gmail.com>
2020-06-15 22:31:13 -0500

    se agrego el archivo main y en el se encuentran los ejemplos de variables y el uso variado del print

---
+++ main.py
@@ -0,0 +1,24 @@
+nombre = "Daniel"
+print (nombre)
+
+numero = "4"
+print(numero)
+
+Numero = "5"
+print(Numero)
+
+felicidad = True
+print(felicidad)
+
+nombre1 = "Daniel"
+apellido = "Rodriguez"
+print(nombre, apellido)
+
+numero1 = 20
+numero2 = 30
+print(numero1, "y", numero2)
+
+nombre2 = "Anita"
+apellido2 = "Mun"
+edad = 21
+print("mi nombre es ", nombre2, "y me apellido", apellido2, "mi edad es ", edad)
