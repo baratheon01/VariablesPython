5955131aff5e769d9d7dd0553cab0b4f323628c6
DanielRB <trynd3@gmail.com>
2020-06-15 22:42:01 -0500

    se añadieron comentarios en el archivo main y a operaciones se modifico todo su contenido

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+#Tipo de dato: String
 nombre = "Daniel"
 print (nombre)
 
+#Tipo de datos: String
 numero = "4"
 print(numero)
 
 Numero = "5"
 print(Numero)
 
+#Tipo de dato: Entero
+Numero1 = 3
+print(Numero1)
+
+#Tipo de dato: Boolean
 felicidad = True
 print(felicidad)
 
+#Forma de imprimir dos datos de tipo string en una sola linea seguidos
+#La coma representa el salto a la siguiente variable que tiene por imprimir
 nombre1 = "Daniel"
 apellido = "Rodriguez"
 print(nombre, apellido)
 
+#Forma de imprimir dos datos de tipo int en una sola linea seguidos
+#Integrando un dato string que no proviene de ninguna variable
 numero1 = 20
 numero2 = 30
 print(numero1, "y", numero2)
 
+#Forma de imprimir 3 datos, siendo dos string y uno entero
+#Integrando de la misma forma datos string que no vienen de nunguna variable
 nombre2 = "Anita"
 apellido2 = "Mun"
 edad = 21
--- operaciones.py
+++ operaciones.py
@@ -1,1 +1,14 @@
+#Suma de dos enteros, de acuerdo a la operacion se usa el debido operador
+num1 = int(4)
+num2 = int(5)
+print(num1 + num2)
+
+i = "Hola"
+Num = 20
+#Forma erronea a la hora de imprimir y unir las partes con el operador +
+print(i+ "" +Num)
+
+#Forma correcta, ya que declarando que el numero es una string, 
+# hace que lo permita leer como tal
+print(i+ " Tengo " + str(Num))
 
